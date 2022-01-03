236a6b78d48bd84c3dd7d0107c7e7a1b3492135a
Peter Peck <karatekid4christ@gmail.com>
2022-01-03 14:14:48 -0800

    Clean up footer component

--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -1,4 +1,4 @@
-import { MDBFooter } from 'mdb-react-ui-kit';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import SocialMediaLinks from '../molecules/SocialMediaLinks';
 
 const Footer = () => {
@@ -13,16 +13,16 @@ const Footer = () => {
 				</div>
 			</section>
 			<section>
-				<div className="container text-center text-md-start mt-5">
-					<div className="row mt-3">
-						<div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+				<MDBContainer className="text-center text-md-start mt-5">
+					<MDBRow className="mt-3">
+						<MDBCol md={3} lg={4} xl={3} className="mx-auto mb-4">
 							<h6 className="text-uppercase fw-bold mb-4">
 								<i className="fa fa-gem me-3"/> Peter Peck Photography
 							</h6>
 							<p>Please reach out to me if you have any requests or inquiries on prices!</p>
 							<p>Be sure to check and follow me on my social meda accounts up above!</p>
-						</div>
-						<div className="col-md-2 col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+						</MDBCol>
+						<MDBCol md={2} lg={2} xl={2} className="mx-auto mb-4">
 							<h6 className="text-uppercase fw-bold mb-4">
 								Services
 							</h6>
@@ -35,8 +35,8 @@ const Footer = () => {
 							<p>
 								Aerial/Drone
 							</p>
-						</div>
-						<div id="contact" className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+						</MDBCol>
+						<MDBCol id="contact" md={4} lg={3} xl={3} className="mx-auto mb-md-0 mb-4">
 							<h6 className="text-uppercase fw-bold mb-4">Contact</h6>
 							<p>
 								<i className="fa fa-home me-3"/> Moscow, ID 83843
@@ -47,9 +47,9 @@ const Footer = () => {
 							<p>
 								<i className="fa fa-phone me-3"/> + 1 406 925 9137
 							</p>
-						</div>
-					</div>
-				</div>
+						</MDBCol>
+					</MDBRow>
+				</MDBContainer>
 			</section>
 			<div className="text-center p-4 rounded" style={{ backgroundColor: '#323741', color: '#FFFFFA' }}>
 				<small>Peter Peck Photography &copy; {(new Date().getFullYear())}</small>
